278ada2054cb48386f367772920d861530a74516
Permata Asa Aitika <asaaitika@gmail.com>
2025-12-16 12:39:28 +0000

    Add configuration and database connection setup
    - Create config package for environment variables
    - Implement database connection with connection pooling
    - Add .env.example template

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
+	ServerPort string
+}
+
+func LoadConfig() (*Config, error) {
+	godotenv.Load()
+
+	config := &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		DBName:     getEnv("DB_NAME", "article_db"),
+		DBSSLMode:  getEnv("DB_SSLMODE", "disable"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+
+	return config, nil
+}
+
+func (c *Config) GetDBConnectionString() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName, c.DBSSLMode,
+	)
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
